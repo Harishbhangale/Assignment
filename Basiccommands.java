de227aed7fc766ab66d77f3a23c5acf8697b979e
Harishbhangale <harish.bhangale.25@gmail.com>
2022-05-16 15:51:35 +0530

    Done

---
+++ Basiccommands.java
@@ -0,0 +1,15 @@
+public class Basiccommands {
+
+	public static void main(String[] args) {
+      
+		int y=11;
+		  ++y;
+		System.out.println(y);  //postfix increment operator  //11  11 
+		
+		 
+		 
+ 		
+
+	}
+
+}
